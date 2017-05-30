bdf21f0f79ae639cfc75cafdc6a74728b5bbebdf
Kyra Fillmore <kfillmore27@gmail.com>
2017-05-30 18:04:37 -0400

    reorg

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts
   post 'authenticate', to: 'authentication#authenticate'
+  resources :posts
   resources :written_things
   resources :things
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
